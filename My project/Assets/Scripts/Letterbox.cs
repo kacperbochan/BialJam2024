500ba1b502f06c33966dc4d02c3c7df7f84e3fd7
pingwin01games <pingwin01games@gmail.com>
2024-05-31 16:35:02 +0200

    Finished letterboxing

--- My project/Assets/Scripts/Letterbox.cs
+++ My project/Assets/Scripts/Letterbox.cs
@@ -21,21 +21,19 @@ public class Letterbox : MonoBehaviour
 
         float x_diff = desired_screen_size.x - current_screen_size.x;
         float y_diff = desired_screen_size.y - current_screen_size.y;
-        x_diff /= 75;
-        y_diff /= 75;
 
-        Vector2 Letterbox_left_pos = Camera.main.ScreenToWorldPoint(new Vector3(0, current_screen_size.y/2, 0));
-        Vector2 Letterbox_right_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x, current_screen_size.y/2, 0));
+        Vector2 Letterbox_left_pos = Camera.main.ScreenToWorldPoint(new Vector3(-x_diff/2, current_screen_size.y/2, 0));
+        Vector2 Letterbox_right_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x + x_diff/2, current_screen_size.y/2, 0));
         Vector2 Letterbox_up_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, current_screen_size.y, 0));
-        Vector3 Letterbox_down_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, 0, 0));
+        Vector2 Letterbox_down_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, 0, 0));
 
         foreach(Letterbox_Collider obj in gameObject.GetComponentsInChildren<Letterbox_Collider>())
         {
             if (obj == null) continue;
-            if (obj.name == "LetterboxLeft") obj.transform.position = new Vector3(Letterbox_left_pos.x - 15 - x_diff, Letterbox_left_pos.y, 10);
-            if (obj.name == "LetterboxRight") obj.transform.position = new Vector3(Letterbox_right_pos.x + 15 + x_diff, Letterbox_right_pos.y, 10);
-            if (obj.name == "LetterboxUp") obj.transform.position = new Vector3(Letterbox_up_pos.x, Letterbox_up_pos.y + 15 + y_diff, 10);
-            if(obj.name == "LetterboxDown") obj.transform.position = new Vector3(Letterbox_down_pos.x, Letterbox_down_pos.y - 15 - y_diff, 10);
+            if (obj.name == "LetterboxLeft") obj.transform.position = new Vector3(Letterbox_left_pos.x - 15, Letterbox_left_pos.y, -29);
+            if (obj.name == "LetterboxRight") obj.transform.position = new Vector3(Letterbox_right_pos.x + 15 , Letterbox_right_pos.y, -29);
+            if (obj.name == "LetterboxUp") obj.transform.position = new Vector3(Letterbox_up_pos.x, Letterbox_up_pos.y + 15, -29);
+            if (obj.name == "LetterboxDown") obj.transform.position = new Vector3(Letterbox_down_pos.x, Letterbox_down_pos.y - 15, -29);
         }
     }
     public static Vector2 getScreenSize()
