bd8cfc2d465d53c89a99f3f6a41b6b840ec467e1
rafal-fundowicz <165918000+rafal-fundowicz@users.noreply.github.com>
2024-06-01 09:29:30 +0200

    letterbox fixed

--- My project/Assets/Scripts/Letterbox.cs
+++ My project/Assets/Scripts/Letterbox.cs
@@ -6,63 +6,56 @@ public class Letterbox : MonoBehaviour
     [SerializeField] private GameObject bottomLetterbox;
     [SerializeField] private GameObject topLetterbox;
     [SerializeField] private Transform clouds;
-    private float targetCameraSize;
+    private const float CAMERA_SIZE_TO_CLOUD_SCALE_RATIO = 5f;
 
     public static Letterbox Instance { get; private set; }
     private void Awake()
     {
         Instance = this;
+        UpdateLetterbox();
     }
     private void OnValidate()
     {
         Instance = this;
-    }
-    private void Start()
-    {
-        targetCameraSize = Camera.main.orthographicSize;
         UpdateLetterbox();
     }
 
     public void UpdateLetterbox()
     {
+        /*
         float width = (float)Screen.width;
         float height = (float)Screen.height;
         float targetRatio = 16f / 9f;
 
-        if (width / height > targetRatio)
-        {
-            // wide screen
-            Camera.main.orthographicSize = targetCameraSize;
-        }
-        else
-        {
-            // tall screen, zoom camera out to keep vertical FOV
-            float sizeMultiplier = targetRatio / (width / height);
-            Camera.main.orthographicSize = targetCameraSize * sizeMultiplier;
-        }
-
         float letterboxWidth = (width - (height * targetRatio)) / 2f;
         float letterboxHeight = (height - (width / targetRatio)) / 2f;
 
+        float desiredWidth = height * targetRatio;
 
-        Vector2 desired_screen_size = new();
-        Vector2 current_screen_size = new(width, height);
+        Vector2 Letterbox_left_pos = Camera.main.ScreenToWorldPoint(new Vector2(-(desiredWidth - width) / 2, height/2));
+        Vector2 Letterbox_right_pos = Camera.main.ScreenToWorldPoint(new Vector2(width + (desiredWidth - width) / 2, height/2));
+        Vector2 Letterbox_up_pos = Camera.main.ScreenToWorldPoint(new Vector2(width/2, height));
+        Vector2 Letterbox_down_pos = Camera.main.ScreenToWorldPoint(new Vector2(width/2, 0));
 
-        desired_screen_size.y = current_screen_size.y;
-        desired_screen_size.x = current_screen_size.y * targetRatio;
+        leftLetterbox.transform.position = new Vector2(Letterbox_left_pos.x - 15, Letterbox_left_pos.y);
+        rightLetterbox.transform.position = new Vector2(Letterbox_right_pos.x + 15, Letterbox_right_pos.y);
+        topLetterbox.transform.position = new Vector2(Letterbox_up_pos.x, Letterbox_up_pos.y + 15);
+        leftLetterbox.transform.position = new Vector2(Letterbox_down_pos.x, Letterbox_down_pos.y - 15);
+        */
+        float cloudsScale = Camera.main.orthographicSize / CAMERA_SIZE_TO_CLOUD_SCALE_RATIO;
+        clouds.position = new Vector3(Camera.main.transform.position.x - 10f * cloudsScale, Camera.main.transform.position.y);
+        clouds.localScale = Vector3.one * cloudsScale;
 
-        float x_diff = desired_screen_size.x - current_screen_size.x;
+        leftLetterbox.transform.localScale = Vector3.one * cloudsScale * 100f;
+        leftLetterbox.transform.position = new Vector3(Camera.main.transform.position.x - cloudsScale * 58.9f, Camera.main.transform.position.y);
 
-        Vector2 Letterbox_left_pos = Camera.main.ScreenToWorldPoint(new Vector3(-x_diff/2, current_screen_size.y/2, 0));
-        Vector2 Letterbox_right_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x + x_diff/2, current_screen_size.y/2, 0));
-        Vector2 Letterbox_up_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, current_screen_size.y, 0));
-        Vector2 Letterbox_down_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, 0, 0));
+        rightLetterbox.transform.localScale = Vector3.one * cloudsScale * 100f;
+        rightLetterbox.transform.position = new Vector3(Camera.main.transform.position.x + cloudsScale * 58.9f, Camera.main.transform.position.y);
 
-        leftLetterbox.transform.position = new Vector3(Letterbox_left_pos.x - 15, Letterbox_left_pos.y, -29);
-        rightLetterbox.transform.position = new Vector3(Letterbox_right_pos.x + 15, Letterbox_right_pos.y, -29);
-        topLetterbox.transform.position = new Vector3(Letterbox_up_pos.x, Letterbox_up_pos.y + 15, -29);
-        leftLetterbox.transform.position = new Vector3(Letterbox_down_pos.x, Letterbox_down_pos.y - 15, -29);
-        
-        clouds.position = new Vector3(Letterbox_up_pos.x-15, Letterbox_up_pos.y-15, -29);
+        topLetterbox.transform.localScale = Vector3.one * cloudsScale * 100f;
+        topLetterbox.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y + cloudsScale * 55f);
+
+        bottomLetterbox.transform.localScale = Vector3.one * cloudsScale * 100f;
+        bottomLetterbox.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y - cloudsScale * 55f);
     }
 }
