64ddf52f64dd600b6ceca1753e5a00baf0bd57fc
rafal-fundowicz <165918000+rafal-fundowicz@users.noreply.github.com>
2024-06-01 08:16:07 +0200

    update letterboxing

--- My project/Assets/Scripts/Letterbox.cs
+++ My project/Assets/Scripts/Letterbox.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
-
 public class Letterbox : MonoBehaviour
 {
+    [SerializeField] private GameObject leftLetterbox;
+    [SerializeField] private GameObject rightLetterbox;
+    [SerializeField] private GameObject bottomLetterbox;
+    [SerializeField] private GameObject topLetterbox;
     [SerializeField] private Transform clouds;
+    private float targetCameraSize;
+
     public static Letterbox Instance { get; private set; }
     private void Awake()
     {
@@ -12,35 +17,52 @@ public class Letterbox : MonoBehaviour
     {
         Instance = this;
     }
+    private void Start()
+    {
+        targetCameraSize = Camera.main.orthographicSize;
+        UpdateLetterbox();
+    }
+
     public void UpdateLetterbox()
     {
-        Vector2 desired_screen_size = new Vector2();
-        Vector2 current_screen_size = getScreenSize();
+        float width = (float)Screen.width;
+        float height = (float)Screen.height;
+        float targetRatio = 16f / 9f;
+
+        if (width / height > targetRatio)
+        {
+            // wide screen
+            Camera.main.orthographicSize = targetCameraSize;
+        }
+        else
+        {
+            // tall screen, zoom camera out to keep vertical FOV
+            float sizeMultiplier = targetRatio / (width / height);
+            Camera.main.orthographicSize = targetCameraSize * sizeMultiplier;
+        }
+
+        float letterboxWidth = (width - (height * targetRatio)) / 2f;
+        float letterboxHeight = (height - (width / targetRatio)) / 2f;
+
+
+        Vector2 desired_screen_size = new();
+        Vector2 current_screen_size = new(width, height);
 
         desired_screen_size.y = current_screen_size.y;
-        desired_screen_size.x = current_screen_size.y / 9 * 16;
+        desired_screen_size.x = current_screen_size.y * targetRatio;
 
         float x_diff = desired_screen_size.x - current_screen_size.x;
-        float y_diff = desired_screen_size.y - current_screen_size.y;
 
         Vector2 Letterbox_left_pos = Camera.main.ScreenToWorldPoint(new Vector3(-x_diff/2, current_screen_size.y/2, 0));
         Vector2 Letterbox_right_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x + x_diff/2, current_screen_size.y/2, 0));
         Vector2 Letterbox_up_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, current_screen_size.y, 0));
         Vector2 Letterbox_down_pos = Camera.main.ScreenToWorldPoint(new Vector3(current_screen_size.x/2, 0, 0));
 
-        foreach(Letterbox_Collider obj in gameObject.GetComponentsInChildren<Letterbox_Collider>())
-        {
-            if (obj == null) continue;
-            if (obj.name == "LetterboxLeft") obj.transform.position = new Vector3(Letterbox_left_pos.x - 15, Letterbox_left_pos.y, -29);
-            if (obj.name == "LetterboxRight") obj.transform.position = new Vector3(Letterbox_right_pos.x + 15 , Letterbox_right_pos.y, -29);
-            if (obj.name == "LetterboxUp") obj.transform.position = new Vector3(Letterbox_up_pos.x, Letterbox_up_pos.y + 15, -29);
-            if (obj.name == "LetterboxDown") obj.transform.position = new Vector3(Letterbox_down_pos.x, Letterbox_down_pos.y - 15, -29);
-        }
-
+        leftLetterbox.transform.position = new Vector3(Letterbox_left_pos.x - 15, Letterbox_left_pos.y, -29);
+        rightLetterbox.transform.position = new Vector3(Letterbox_right_pos.x + 15, Letterbox_right_pos.y, -29);
+        topLetterbox.transform.position = new Vector3(Letterbox_up_pos.x, Letterbox_up_pos.y + 15, -29);
+        leftLetterbox.transform.position = new Vector3(Letterbox_down_pos.x, Letterbox_down_pos.y - 15, -29);
+        
         clouds.position = new Vector3(Letterbox_up_pos.x-15, Letterbox_up_pos.y-15, -29);
-    }
-    private Vector2 getScreenSize()
-    {
-        return new Vector2(Screen.width, Screen.height);
     }
 }
