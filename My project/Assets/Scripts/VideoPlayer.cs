392f9772e7f4c8e8004e0c3507fc09d628710c61
rafal-fundowicz <165918000+rafal-fundowicz@users.noreply.github.com>
2024-06-04 12:34:55 +0200

    changed video mode to support webgl

--- My project/Assets/Scripts/VideoPlayer.cs
+++ My project/Assets/Scripts/VideoPlayer.cs
@@ -5,6 +5,9 @@ public class VideoPlayer : MonoBehaviour
 {
     private void Start()
     {
+        UnityEngine.Video.VideoPlayer videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "ending_animation.mp4");
+        videoPlayer.Play();
         GetComponent<UnityEngine.Video.VideoPlayer>().loopPointReached += VideoPlayer_loopPointReached;
     }
 
