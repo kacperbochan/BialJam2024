ea87f41ff9f590c56e7e588449d88427ce811e44
rafal-fundowicz <165918000+rafal-fundowicz@users.noreply.github.com>
2024-06-14 21:23:22 +0200

    added cooldown for castor event

--- My project/Assets/Scripts/Castor.cs
+++ My project/Assets/Scripts/Castor.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 public class Castor : MonoBehaviour
 {
     public static event EventHandler OnCastorCollision;
+    private static float lastCastorEvent = Mathf.NegativeInfinity;
+    [SerializeField] private float castorEventCooldown = 5.0f;
     public static void ResetStaticData()
     {
         OnCastorCollision = null;
+        lastCastorEvent = Mathf.NegativeInfinity;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        OnCastorCollision?.Invoke(this, EventArgs.Empty);
+        if (TimeSince(lastCastorEvent) > castorEventCooldown)
+        {
+            OnCastorCollision?.Invoke(this, EventArgs.Empty);
+            lastCastorEvent = Time.time;
+        }
+    }
+    private float TimeSince(float timePoint)
+    {
+        return Time.time - timePoint;
     }
 }
